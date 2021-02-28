b0309479ec70456b7d98c992feacf1226d2a26f5
Paul Joseph Devlin <devunderslash@users.noreply.github.com>
2021-02-28 23:40:29 +0000

    Terraform added ALB to ECS Service

--- main.tf
+++ main.tf
@@ -78,6 +78,13 @@ resource "aws_ecs_service" "my_first_service" {
   launch_type     = "FARGATE"
   desired_count   = 3 # Setting the number of containers we want deployed to 3
 
+#Add Load Balancer to Service after ALB Creation to link to ECS
+load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn # Referencing our target group
+    container_name   = aws_ecs_task_definition.ecs_task.family # Task Name
+    container_port   = 3000 # Specifying the container port
+  }
+
 #Added the below to reference the subnets added
   network_configuration {
     subnets          = [aws_default_subnet.default_subnet_a.id, aws_default_subnet.default_subnet_b.id, aws_default_subnet.default_subnet_c.id]
